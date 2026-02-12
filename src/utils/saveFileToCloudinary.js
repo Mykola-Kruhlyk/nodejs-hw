9a12f8fe03f4336bdca8be877b22728c137cb4b3
unknown <mykola.kruhlyk.pl@gmail.com>
2026-02-12 19:28:39 +0100

    my 5th node wh

---
+++ src/utils/saveFileToCloudinary.js
@@ -0,0 +1,32 @@
+import {
+    v2 as cloudinary
+} from "cloudinary";
+import {
+    Readable
+} from "node:stream";
+
+cloudinary.config({
+    secure: true,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+
+
+export const saveFileToCloudinary = async (buffer) => {
+
+    return new Promise((resolve, reject) => {
+        const uploadStream = cloudinary.uploader.upload_stream({
+            folder: "user-avatars",
+            overwrite: true,
+            resource_type: "image",
+            unique_filename: true,
+            use_filename: true,
+        }, (error, result) => {
+            error ? reject(error) : resolve(result);
+        });
+
+        Readable.from(buffer).pipe(uploadStream);
+    });
+};
